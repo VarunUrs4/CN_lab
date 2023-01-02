a61e3c64738e0071a34c0c3042ec35f8ff01c019
VarunUrs4 <84217264+VarunUrs4@users.noreply.github.com>
2023-01-02 12:24:37 +0530

    Add files via upload

---
+++ Leakybucket.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+void main()
+{
+    int bucketSize = 60;
+    int inputRate = 0;
+    int outputRate = 0;
+    int remainingSize = 0;
+    int dataPresent = -1;
+
+    printf("\n Enter the size of the bucket : ");
+    scanf("%d",&bucketSize);
+
+    printf("\n Enter the output flow rate : ");
+    scanf("%d",&outputRate);
+
+    while(1)
+    {
+        printf("\n Do you have any data packets? (1 or 0): ");
+        scanf("%d",&dataPresent);
+
+        if(dataPresent == 1)
+        {
+            printf("\n Enter the input data flow rate : ");
+            scanf("%d",&inputRate);
+
+            if((remainingSize + inputRate) <= bucketSize)
+            {
+                remainingSize += inputRate;
+
+                printf("\n The capacity of bucket (before output flow) : %d",remainingSize);
+                remainingSize -= outputRate;
+                printf("\n The capacity of bucket now : %d\n",remainingSize);
+            }
+            else
+            {
+                printf("\n Bucket is Full!!!!!!! Cannot accept the input!!!");
+                printf("\n The capacity of bucket now : %d\n",remainingSize);
+
+            }
+        }
+
+        else
+        {
+            printf("\n The capacity of bucket (before output flow) : %d",remainingSize);
+            remainingSize -= outputRate;
+            printf("\n The capacity of bucket now : %d\n",remainingSize);
+        }
+    }
+}
