924893a995473bc68c38026e3bf8eadd85c4c161
VarunUrs4 <84217264+VarunUrs4@users.noreply.github.com>
2023-01-29 22:26:02 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from socket import *
 serverName='DESKTOP-KGIIO2U'
 serverPort=14000
-clientSocket=socket(AF_INET,SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
-sentence=input("Enter file name: ")
-clientSocket.send(sentence.encode())
-filecontents=clientSocket.recv(1024).decode()
-print('From Server:',filecontents)
-clientSocket.close()
+serverSocket=socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((serverName,serverPort))
+serverSocket.listen(1)
+print ("The server is ready to receive")
+while 1:
+    connectionSocket,addr=serverSocket.accept()
+    sentence=connectionSocket.recv(1024).decode()
+    file=open(sentence,"r")
+    l=file.read(1024)
+    connectionSocket.send(l.encode())
+    file.close()
+    connectionSocket.close()
